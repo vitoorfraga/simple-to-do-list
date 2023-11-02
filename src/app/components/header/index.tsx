7acc86e191af04dda73602bbcc6d4ed2b8a0c040
Vitor Fraga <vitorfragaps@gmail.com>
2023-11-02 15:38:51 -0300

    feat: create Header component

---
+++ src/app/components/header/index.tsx
@@ -0,0 +1,9 @@
+export function Header() {
+  return (
+    <header className="text-lg border-b-[1px] border-slate-700 h-16 flex justify-center items-center uppercase font-bold text-slate-300">
+      <h1>
+        Simple <span className="text-amber-400">To Do</span> List
+      </h1>
+    </header>
+  )
+}
