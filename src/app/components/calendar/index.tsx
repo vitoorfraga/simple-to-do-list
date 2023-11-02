6c77d5d558f37576483ffe3d2a10851c6cc2b49f
Vitor Fraga <vitorfragaps@gmail.com>
2023-11-02 16:49:20 -0300

    feat: create Calendar component

---
+++ src/app/components/calendar/index.tsx
@@ -0,0 +1,15 @@
+export function Calendar() {
+  return (
+    <div className="flex justify-between items-center">
+      <div className="flex gap-2">
+        <p className="font-semibold text-5xl">02</p>
+        <div>
+          <p className="font-semibold">JAN</p>
+          <p>2023</p>
+        </div>
+      </div>
+
+      <p>QUINTA-FEIRA</p>
+    </div>
+  )
+}
