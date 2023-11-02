eebfcc393a16d45464514905e2f08efdac3a32fe
Vitor Fraga <vitorfragaps@gmail.com>
2023-11-02 18:03:45 -0300

    feat: add currentDate to Calendar component

--- src/app/components/calendar/index.tsx
+++ src/app/components/calendar/index.tsx
@@ -1,15 +1,50 @@
 export function Calendar() {
+  // function get
+
+  function getCurrentDate() {
+    const fullCurrentDate = new Date()
+
+    const dayOptions: Intl.DateTimeFormatOptions = {
+      weekday: 'long',
+      timeZone: 'UTC',
+    }
+
+    const monthOptions: Intl.DateTimeFormatOptions = {
+      month: 'long',
+      timeZone: 'UTC',
+    }
+    const dayOfWeek: string = fullCurrentDate.toLocaleDateString(
+      'en',
+      dayOptions,
+    )
+
+    const monthIndex = new Date().getMonth()
+    const monthName = new Date(
+      new Date().getFullYear(),
+      monthIndex,
+      1,
+    ).toLocaleDateString(undefined, monthOptions)
+
+    const day = fullCurrentDate.getDate().toString().padStart(2, '0')
+    const month = monthName
+    const year = fullCurrentDate.getFullYear()
+    return { day, month, year, dayOfWeek }
+  }
+
+  const currentDate = getCurrentDate()
+  console.log(currentDate)
+
   return (
     <div className="flex justify-between items-center">
       <div className="flex gap-2">
-        <p className="font-semibold text-5xl">02</p>
+        <p className="font-semibold text-5xl">{currentDate.day}</p>
         <div>
-          <p className="font-semibold">JAN</p>
-          <p>2023</p>
+          <p className="font-semibold">{currentDate.month}</p>
+          <p>{currentDate.year}</p>
         </div>
       </div>
 
-      <p>QUINTA-FEIRA</p>
+      <p>{currentDate.dayOfWeek.toUpperCase()}</p>
     </div>
   )
 }
