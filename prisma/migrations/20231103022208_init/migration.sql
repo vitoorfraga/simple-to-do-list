2ac4cf3287bb55e3d1e4e5153e08653d210b36b2
Vitor Fraga <vitorfragaps@gmail.com>
2023-11-02 23:26:00 -0300

    chore: initial Task model

---
+++ prisma/migrations/20231103022208_init/migration.sql
@@ -0,0 +1,7 @@
+-- CreateTable
+CREATE TABLE "Task" (
+    "id" INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT,
+    "description" TEXT NOT NULL,
+    "done" BOOLEAN NOT NULL DEFAULT false,
+    "createdAt" DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP
+);
