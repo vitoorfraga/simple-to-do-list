ddffdd32769b4d14827432389abed445781bdedc
Vitor Fraga <vitorfragaps@gmail.com>
2023-11-03 20:54:00 -0300

    style: add disabled style to Button component

--- src/components/button/index.tsx
+++ src/components/button/index.tsx
@@ -11,15 +11,16 @@ export default function Button({
   ...rest
 }: ButtonProps) {
   const defaultStyles =
-    'flex gap-3 py-1 px-3 rounded-md items-center text-base transition delay-20'
+    'flex gap-3 py-1 px-3 rounded-md items-center text-base transition delay-20 disabled:opacity-60 disabled:cursor-not-allowed'
 
   function getVariantStyles(
     variant: 'primary' | 'outline' | 'confirm' | 'cancel',
   ) {
     const variants = {
       primary: 'bg-cyan-900 hover:bg-cyan-800',
-      outline: 'bg-none border-[1px] border-slate-600 hover:bg-cyan-900',
-      confirm: 'text-slate-950 bg-green-500 hover:bg-green-400 ',
+      outline:
+        'bg-none border-[1px] border-slate-600 :not:disabled:hover:bg-cyan-900',
+      confirm: 'text-slate-950 bg-green-500 :not:disabled:hover:bg-green-400 ',
       cancel: 'text-slate-200 bg-red-500 hover:bg-red-400',
     }
 
