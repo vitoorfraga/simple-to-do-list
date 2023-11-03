3845718f4867b85e0af5980aeb6fa34e60ef775b
Vitor Fraga <vitorfragaps@gmail.com>
2023-11-03 00:48:17 -0300

    chore: migrate to postgresql

--- prisma/migrations/20231103022208_init/migration.sql
+++
@@ -1,7 +0,0 @@
--- CreateTable
-CREATE TABLE "Task" (
-    "id" INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT,
-    "description" TEXT NOT NULL,
-    "done" BOOLEAN NOT NULL DEFAULT false,
-    "createdAt" DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP
-);
