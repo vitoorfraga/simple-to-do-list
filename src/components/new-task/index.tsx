4cd58cf43f79dcfb32863eae6888b76c36575039
Vitor Fraga <vitorfragaps@gmail.com>
2023-11-03 15:53:16 -0300

    fix: resolve hydration problems in NewTask component

--- src/components/new-task/index.tsx
+++ src/components/new-task/index.tsx
@@ -3,11 +3,27 @@
 import * as Dialog from '@radix-ui/react-dialog'
 import Button from '../button'
 import { TextField } from '../text-field'
+import { createTask } from '@/actions/new-task'
+import { useSession } from 'next-auth/react'
+import { FormEvent, useState } from 'react'
 
 export const NewTask = () => {
+  const { data } = useSession()
+  const [taskDescription, setTaskDescription] = useState('')
+
+  const handleCreateTaskClick = async (event: FormEvent) => {
+    event.preventDefault()
+
+    if (!data?.user) {
+      // Redireciona para o login
+      return
+    }
+    await createTask(taskDescription, (data?.user).id)
+  }
+
   return (
     <Dialog.Root>
-      <Dialog.Trigger>
+      <Dialog.Trigger asChild>
         <Button variant="primary">New task</Button>
       </Dialog.Trigger>
       <Dialog.Portal>
@@ -17,20 +33,26 @@ export const NewTask = () => {
             <h3 className="text-lg font-bold">Create a new task</h3>
           </Dialog.Title>
 
-          <form className="mt-4">
+          <form
+            className="mt-4"
+            onSubmit={(event) => handleCreateTaskClick(event)}
+          >
             <TextField
               label="Description:"
               name="description"
               placeholder="type your task description"
+              value={taskDescription}
+              onChange={({ target }) => setTaskDescription(target.value)}
             />
-          </form>
 
-          <div className="mt-4 flex items-center justify-end gap-2">
-            <Dialog.Close>
-              <Button variant="cancel">Cancel</Button>
-            </Dialog.Close>
-            <Button variant="confirm">Confirm</Button>
-          </div>
+            <div className="mt-4 flex items-center justify-end gap-2">
+              <Dialog.Close asChild>
+                <Button variant="cancel">Cancel</Button>
+              </Dialog.Close>
+
+              <Button variant="confirm">Confirm</Button>
+            </div>
+          </form>
         </Dialog.Content>
       </Dialog.Portal>
     </Dialog.Root>
