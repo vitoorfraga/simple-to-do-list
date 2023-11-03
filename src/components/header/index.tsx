8198ecca22ab8445c5be8a82f90f8532040c26ef
Vitor Fraga <vitorfragaps@gmail.com>
2023-11-03 00:51:33 -0300

    feat: add Drawer component to header

--- src/components/header/index.tsx
+++ src/components/header/index.tsx
@@ -1,10 +1,10 @@
-import { MenuIcon } from 'lucide-react'
 import { NewTask } from '../new-task'
+import { Drawer } from '../drawer'
 
 export function Header() {
   return (
     <header className="text-lg border-b-[1px] border-slate-700 h-16 flex justify-between items-center uppercase text-slate-300 px-4">
-      <MenuIcon />
+      <Drawer />
       <h1>
         Simple <span className="text-amber-400  font-bold">To Do</span> List
       </h1>
