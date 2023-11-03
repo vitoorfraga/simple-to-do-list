e3555f8fa9739d8a53a2d995188772ef7227db22
Vitor Fraga <vitorfragaps@gmail.com>
2023-11-03 00:51:56 -0300

    feat: add AuthProvider to app layout

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from 'next'
 import { Inter } from 'next/font/google'
 import '@/styles/globals.css'
 import { Header } from '../components/header'
+import { AuthProvider } from '@/providers/auth'
 
 const inter = Inter({ subsets: ['latin'] })
 
@@ -17,8 +18,10 @@ export default function RootLayout({
   return (
     <html lang="pt-br">
       <body className={`${inter.className}flex h-full flex-col`}>
-        <Header />
-        <main className="flex-1 p-4 mt-2">{children}</main>
+        <AuthProvider>
+          <Header />
+          <main className="flex-1 p-4 mt-2">{children}</main>
+        </AuthProvider>
       </body>
     </html>
   )
