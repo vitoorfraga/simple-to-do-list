48c94b6411a4a5684155ad4ea125f5ad40aef5cf
Vitor Fraga <vitorfragaps@gmail.com>
2023-11-05 13:00:36 -0300

    feat: get user tasks on TasksList component

--- src/app/(home)/components/TasksList.tsx
+++ src/app/(home)/components/TasksList.tsx
@@ -1,9 +1,40 @@
-import { Task } from '@/components/task'
+import { Task as TaskCard } from '@/components/task'
+import { authOptions } from '@/lib/auth'
+import { prismaClient } from '@/lib/prisma'
+import { getServerSession } from 'next-auth'
+
+export const dynamic = 'force-dynamic'
 
 export const TasksList = async () => {
+  const session = await getServerSession(authOptions)
+  console.log(session)
+
+  const tasks = await prismaClient.task.findMany({
+    where: {
+      userId: session?.user.id,
+    },
+  })
+
+  if (!session) {
+    return (
+      <p className="text-center flex-1 bg-[red] h-full max-h-full">
+        É necessário estar logado.
+      </p>
+    )
+  }
+
   return (
     <section className="flex flex-col gap-3 mt-8">
-      <Task id="1" name="Criar banco de dados" isChecked={true} />
+      {tasks.map((task) => {
+        return (
+          <TaskCard
+            key={task.id}
+            id={task.id}
+            name={task.description}
+            isChecked={task.done}
+          />
+        )
+      })}
     </section>
   )
 }
