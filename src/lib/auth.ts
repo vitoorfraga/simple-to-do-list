958887092b62f1607884a788950ba1b6ecbae5ce
Vitor Fraga <vitorfragaps@gmail.com>
2023-11-05 14:21:42 -0300

    build: remove unused param os authOptions setup

--- src/lib/auth.ts
+++ src/lib/auth.ts
@@ -12,7 +12,7 @@ export const authOptions: AuthOptions = {
     }),
   ],
   callbacks: {
-    async session({ session, token, user }) {
+    async session({ session, user }) {
       session.user = { ...session.user, id: user.id } as {
         id: string
         name: string
