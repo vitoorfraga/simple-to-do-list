86b301323708d8554adac548f7015c500f4b7a4b
Vitor Fraga <vitorfragaps@gmail.com>
2023-11-25 12:33:52 -0300

    feat: create TasksList component with finished tasks

---
+++ src/app/finished/components/TasksList.tsx
@@ -0,0 +1,43 @@
+import { Task as TaskCard } from '@/components/task'
+import { authOptions } from '@/lib/auth'
+import { prismaClient } from '@/lib/prisma'
+import { getServerSession } from 'next-auth'
+import { Toaster } from 'sonner'
+
+export const dynamic = 'force-dynamic'
+
+export const TasksList = async () => {
+  const session = await getServerSession(authOptions)
+
+  const tasks = await prismaClient.task.findMany({
+    where: {
+      userId: session?.user.id,
+      done: true,
+    },
+  })
+
+  if (!session) {
+    return (
+      <p className="text-center flex-1 mt-16 h-full max-h-full">
+        You need to be logged in.
+      </p>
+    )
+  }
+
+  return (
+    <section className="flex flex-col gap-3 mt-8">
+      {tasks.map((task) => {
+        return (
+          <TaskCard
+            key={task.id}
+            id={task.id}
+            name={task.description}
+            isChecked={task.done}
+          />
+        )
+      })}
+
+      <Toaster />
+    </section>
+  )
+}
